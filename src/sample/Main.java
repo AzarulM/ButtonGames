4fd38117e6435a4c26e0e3fe7fcbf723e52f3796
BT_1N3_30@bktech.local <BT_1N3_30@bktech.local>
2019-01-02 11:42:36 -0500

    Initial commit

---
+++ src/sample/Main.java
@@ -0,0 +1,36 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+
+public class Main extends Application  {
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Button Experiment 1");
+
+
+        Button button = new Button("Button");
+
+        button.setStyle( "-fx-font-size: 2em;");
+        
+        HBox hbox = new HBox(button);
+
+
+        Scene scene = new Scene(hbox, 400, 300);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
